5c7974bcbdb9afffaf08b0c6c134cc7d6f5ff16a
micferna <122208805+micferna@users.noreply.github.com>
2024-09-17 14:34:54 +0200

    Create message.py

---
+++ message.py
@@ -0,0 +1,20 @@
+# message.py
+MESSAGE = """## :video_game: **Bienvenue sur le serveur Discord officiel d'** Epitaphe_TV ! :video_game:
+:star2: **Rejoignez notre communauté dynamique** où le gaming, le fun et les récompenses sont au rendez-vous ! :star2:
+:link: **Lien d'invitation** : https://discord.gg/epi
+:crown: **Pourquoi nous rejoindre ?** :crown:
+- :gift: **Drops exclusifs** : Nitro, comptes premium, et bien plus encore !
+- :joystick: **Sessions de streaming** en direct avec **Epitaphe_TV**.
+- :gem: **Accès VIP** pour des avantages et des drops encore plus exclusifs.
+- :speech_balloon: **Communauté accueillante** et passionnée de jeux vidéo.
+:link: **Retrouvez-moi également sur :**
+- [Twitch](https://www.twitch.tv/epitaphe_tv) :movie_camera:
+- [YouTube](https://www.youtube.com/@epitaphe_tv) :clapper:
+- [TikTok](https://www.tiktok.com/@epitaphe_tv) :mobile_phone:
+- [Kick](https://kick.com/epitaphe-tv) :video_game:
+:bell: **Ne manquez rien !** Activez les notifications pour être le premier à profiter des drops et des événements spéciaux.
+---
+**Prêt à rejoindre l'aventure ?** :tada: Clique sur le lien et rejoins-nous maintenant ! :rocket:
+En vous remerciant par avance à tous !
+**Drop souvent des nitros et des giveaway d'argent sur le Discord 5,10,25,50 euro**
+:rocket: :rocket: :rocket: :rocket: :rocket: :rocket: :rocket: :rocket: :rocket: :rocket: :rocket: :rocket: :rocket: :rocket: :rocket: :rocket: :rocket: :rocket: :rocket: :rocket: :rocket: :rocket: :rocket:"""
